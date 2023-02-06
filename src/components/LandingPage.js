c308acf3950e55a4a3a3962ee6b4295403fd29c3
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-02-06 13:27:30 -0500

    initial commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import { Link } from "react-router-dom"
+
+const LandingPage = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-4">
+          <h3>Blog post 1</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div className="col-sm-4">
+          <h3>Blog post 2</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div className="col-sm-4">
+          <h3>Blog post 3</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default LandingPage
+
+
