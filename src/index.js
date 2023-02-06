95817af93d661c2ecb2d4901a49f36cdf2d98583
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-02-06 13:55:55 -0500

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './components/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
