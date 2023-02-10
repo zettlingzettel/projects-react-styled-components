1f0f71fd0acf5f4ab2067548d8871949d1ef8e85
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-02-10 11:39:00 -0500

    update

---
+++ src/components/BlogComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const BlogComponent = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-4">
+          <h3>Blog Component -- Blog post 1</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div className="col-sm-4">
+          <h3>Blog post 2</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div className="col-sm-4">
+          <h3>Blog post 3</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default  BlogComponent
