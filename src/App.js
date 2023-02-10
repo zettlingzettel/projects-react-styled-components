69d2c7c8fef03c63e22a96bf56d5fc5abd6cc9da
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-02-10 11:47:33 -0500

    updating repo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
-import { Routes, Route } from "react-router-dom";
-
-import LandingPage from './components/LandingPage';
-
-import BlogComponent from './components/BlogComponent.js'
-import WishListComponent from './components/WishListComponent';
-
-import Project1 from './components/projects/project-1/Project1';
-
-// import Main from './project-1/Main.js';
-
-// import RouterComponent from "./components/RouterComponent"
+import Router from "./components/Router"
 
 function App() {
   return (
-    <Routes>
-          <Route exact path ="/" element={<LandingPage />} />
-          <Route exact path ="/project1" element={<Project1 />} />
-          {/* <Route exact path ="/project1-app" element={<Main />} /> */}
-          <Route exact path ="/blog" element={<BlogComponent />} />
-          <Route exact path ="/wishlist" element={<WishListComponent />} />
-    </Routes>
+    <div className="App">
+      <Router />
+    </div>
   );
 }
 
