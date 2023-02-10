13241c969eb13da1bb1da647e9302c39a79a98f1
ANNA SMOLENOVA <blare-lambdas-0t@icloud.com>
2023-02-10 11:58:24 -0500

    updated content

--- src/components/WishListComponent.js
+++ src/components/WishListComponent.js
@@ -1,32 +1,11 @@
 import React from 'react'
-import { Link } from "react-router-dom"
 
-const LandingPage = (props) => {
+const WishListComponent = (props) => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-4">
-          <h3>Blog post 1</h3>
-          Update
-          {/* <p> <Link to={'/project1'}>
-        Click me to go to Project 1
-      </Link></p> */}
-      {/* <p> <Link to={'/project1-app'}>
-        Click me to go to Project 1 App
-      </Link> */}
-      <p> <Link to={'/blog'}>
-        Click me to go to Blog
-      </Link></p>
-      <p> <Link to={'/wishlist'}>
-        Click me to go to WishList
-      </Link></p>
-
-      <p> <Link to={'/project1'}>
-        Click me to go to Project 1
-      </Link></p>
-
-
-          {/* </p> */}
+          <h3>Wishlist Blog post 1</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
         <div className="col-sm-4">
@@ -41,6 +20,4 @@ const LandingPage = (props) => {
     </div>
   )
 }
-export default LandingPage
-
-
+export default WishListComponent
--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path="/*" element={ <App /> }>
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
